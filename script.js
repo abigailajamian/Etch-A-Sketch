549b62412da321f1a3daab6a1b243f80ee57862f
Abigail Ajamian <abigailajamian@gmail.com>
2023-02-17 10:26:19 -0500

    finish basic set size and grid color change

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16,1fr)";
+let size = document.querySelector(".size");
+size.addEventListener('click', setSize);
+function setSize(){
+    let input = prompt("What size grid would you like?");
+    if(input<=100){
+        createGrid(input);
+    } else{
+        console.log('Please pick a number less than or equal to 100');
+    }
+}
 
-for (let i = 0; i<256; i++){
-    let square= document.createElement("div");
-    square.style.backgroundColor="blue";
-    board.insertAdjacentElement('beforeend',square);
+
+function createGrid(num){
+    resetGrid();
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    
+    for (let i = 0; i<num*num; i++){
+        let square= document.createElement("div");
+        square.style.backgroundColor="gray";
+        board.insertAdjacentElement('beforeend',square);
+        square.classList.add("boxes");
+    }
+    
+    
+    let boxes=document.getElementsByClassName("boxes");
+    for(let i= 0; i<boxes.length;i++){
+        boxes[i].addEventListener('mouseover',mouseOver);
+    }
+}
+
+createGrid(16);
+
+function mouseOver(){
+    this.style.backgroundColor = "white";
+}
+
+function resetGrid (){
+    let boxes=document.getElementsByClassName("boxes");
+    while(boxes.length>0){
+        let boxes=document.getElementsByClassName("boxes");
+        boxes[0].remove();
+    }
 }
